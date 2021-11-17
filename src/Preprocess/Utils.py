9bf8c5481d5adf4945337a47df5162618888f480
Inigo Lopez Gazpio <inigo.lopezgazpio@deusto.es>
2021-11-17 15:24:12 +0100

    BUGFIX #10 - stopwords

--- src/Preprocess/Utils.py
+++ src/Preprocess/Utils.py
@@ -8,10 +8,28 @@ from nltk.stem import WordNetLemmatizer
 PREPROCESSING UTILITIES 
 """
 
+lemmatizer = WordNetLemmatizer()
+
+
 def readDataset(path : str, nrows=None):
     df = pd.read_csv(path, delimiter="\t", header=None, nrows=nrows)
     df.columns = ['STS', 'NLI', 'left', 'right', 'left_num', 'right_num', 'id']
     return df
+
+
+def saveDatasetCSV(df : pd.DataFrame, savePath : str, sep = ","):
+    df.to_csv( savePath, sep=sep )
+
+
+def readDatasetCSV(path : str, sep = ",") -> pd.DataFrame:
+    return pd.read_csv( path, sep=sep )
+
+
+def saveDatasetPickle(df : pd.DataFrame, savePath : str):
+    df.to_pickle( savePath )
+
+def readDatasetPickle( path : str) -> pd.DataFrame:
+    return pd.read_pickle( path )
 
 # STRIP
 def addColumnsStrip(df : pd.DataFrame):
@@ -30,18 +48,17 @@ def addColumnsNoPunctuations(df : pd.DataFrame):
 
 # LEMMATIZED
 def addColumnsLemmatized(df : pd.DataFrame):
-    lemmatizer = WordNetLemmatizer()
     for current in ["left", "right"]:
         df [current + '_strip_tokenized_noPunct_lemmat']= df[current + '_strip_tokenized_noPunct'].apply(lambda x: [lemmatizer.lemmatize(w) for w in x])
 
 # NO STOP WORDS (CONTENT WORDS)
 def addColumnsContentWords(df : pd.DataFrame):
     for current in ["left", "right"]:
-        df[current + '_strip_tokenized_noPunct_lemmat_noStopWords'] = df [current + '_strip_tokenized_noPunct_lemmat'].apply(lambda x: [word for word in x if not word in stopwords.words()])
+        df[current + '_strip_tokenized_noPunct_lemmat_noStopWords'] = df [current + '_strip_tokenized_noPunct_lemmat'].apply(lambda x: [word for word in x if not word in stopwords.words('english')])
 
  # STOP WORDS
 def addColumnsStopWords(df : pd.DataFrame):
     for current in ["left", "right"]:
-        df[current + '_strip_tokenized_noPunct_StopWords'] = df [current + '_strip_tokenized_noPunct'].apply(lambda x: [word for word in x if word in stopwords.words()])
+        df[current + '_strip_tokenized_noPunct_StopWords'] = df [current + '_strip_tokenized_noPunct'].apply(lambda x: [word for word in x if word in stopwords.words('english')])
 
 
