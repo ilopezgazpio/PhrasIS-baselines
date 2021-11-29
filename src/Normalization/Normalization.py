8d327c2182ace0058e722d4af55f503ca7f0dede
Inigo Lopez Gazpio <inigo.lopezgazpio@deusto.es>
2021-11-29 18:05:33 +0100

    minor text correction

--- src/Normalization/Normalization.py
+++ src/Normalization/Normalization.py
@@ -4,12 +4,12 @@ from sklearn import preprocessing
 
 
 def standardNormalization (df: pd.DataFrame, columnsNormalize = ALL_FEATURES):
-    ''' Normalize given columns of the dataframe using a standard scales '''
+    ''' Normalize given columns of the dataframe using a standard scaler '''
     X = preprocessing.StandardScaler().fit_transform( df[columnsNormalize].values.astype(float))
     df.update(pd.DataFrame(X, columns=columnsNormalize))
 
 
 def miniMaxNormalization (df: pd.DataFrame, columnsNormalize = ALL_FEATURES):
-    ''' Normalize given columns of the dataframe using a standard scales '''
+    ''' Normalize given columns of the dataframe using a MinMax scaler '''
     X = preprocessing.MinMaxScaler().fit_transform( df[columnsNormalize].values.astype(float))
     df.update(pd.DataFrame(X, columns=columnsNormalize))
