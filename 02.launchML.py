d150a80422858eb5ce18298aab16758db6329b7e
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2022-02-18 12:19:34 +0100

    Refractor and major changes on CrossValidation

--- 02.launchML.py
+++ 02.launchML.py
@@ -11,6 +11,15 @@ from src.MachineLearning import MachineLearning
 from src.Constants.Constants import ALL_FEATURES
 from src.Constants.Constants import LEXICAL_COLS
 
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+
 # Set seed for all libraries
 np.random.seed(123)
 
@@ -20,108 +29,114 @@ pd.options.display.max_columns= None
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 100)
 
-# Step 1 -> load datasets
-names = [
+# Step 1 -> load all datasets
+
+datasetsFolder = "dataset/bin"
+if not os.path.exists(datasetsFolder):
+    sys.exit("First preprocess the datasets... Exiting")
+
+all_names = [
     'PhrasIS_train_h_n',
     'PhrasIS_train_h_p',
     'PhrasIS_train_i_n',
     'PhrasIS_train_i_p',
+    'PhrasIS_test_h_n',
+    'PhrasIS_test_h_p',
+    'PhrasIS_test_i_n',
+    'PhrasIS_test_i_p',
 ]
 
-saveFolder = "dirty"
-if not os.path.exists(saveFolder):
-    sys.exit("First preprocess the datasets... Exiting")
+all_datasets = dict( {name : Utils.loadDatasetPickle( os.path.join(datasetsFolder, name+".pickle")) for name in all_names })
 
-datasets = dict( {name : Utils.loadDatasetPickle( os.path.join(saveFolder, name+".pickle")) for name in names })
+# Step 2 -> create Bag of Word features with CountVectorizer (we need same number of features for all datasets, so we share CountVectorizer instance for all datasets)
+cv = CountVectorizer()
+common_strings = pd.DataFrame([], columns=['full_text'])
 
-# Step 2 -> instantiate models
-columns_of_X = ['left', 'right']
-column_y_sts = ['STS']
-column_y_nli= ['NLI']
+for name, dataset in all_datasets.items():
+    common_strings = pd.concat( [common_strings, pd.DataFrame(dataset['left'] + "," + dataset['right'], columns=['full_text'])], axis=0, ignore_index=True)
 
-scores_sts_de=[]
-scores_nli_de=[]
-scores_sts_kn=[]
-scores_nli_kn=[]
-scores_sts_lr=[]
-scores_nli_lr=[]
-scores_sts_nb=[]
-scores_nli_nb=[]
-scores_sts_svm=[]
-scores_nli_svm=[]
+# fit cv to get to know all words of all datasets
+cv.fit(common_strings['full_text'])
+# Check correctness with vc.vocabulary_
 
-scores = {
-    "accuracy" : "accuracy",
-    "f1_macro" : "f1_macro"
-}
+# Step 3 -> Process datasets, add BoW features with global countvectorizer and remove labels
 
-for name, dataset in datasets.items():
-    X_strings = dataset[columns_of_X]
-    X_strings['cv'] = X_strings['left'] + "," + X_strings['right']
-    cv = CountVectorizer()
-    X_bow = cv.fit_transform(X_strings['cv'])
-    X_train = np.concatenate((X_bow.toarray(), dataset[ALL_FEATURES].values), axis=1)
-    y_sts = dataset[column_y_sts].values
-    y_nli =dataset[column_y_nli].values
+# Now use trained cv to create BOW features in each dataset
+target_sts = dict()
+target_nli = dict()
 
-# Step 3 -> train models
-    for col in scores:
-        medias_sts_de, medias_nli_de=MachineLearning.DecisionTree(X_train, y_sts, y_nli, col)
-        scores_sts_de.append(medias_sts_de)
-        scores_nli_de.append(medias_nli_de)
+for name, dataset in all_datasets.items():
+    target_sts[name] = dataset['STS'].values
+    target_nli[name] = dataset['NLI'].values
 
-        medias_sts_knn, medias_nli_knn= MachineLearning.Knn(X_train, y_sts, y_nli, col)
-        scores_sts_kn.append(medias_sts_knn)
-        scores_nli_kn.append(medias_nli_knn)
+    # Remove supervised labels for training
+    all_datasets[name].drop(columns = ['STS','NLI'])
 
-        medias_sts_lr, medias_nli_lr=MachineLearning.LR(X_train, y_sts, y_nli, col)
-        scores_sts_lr.append(medias_sts_lr)
-        scores_nli_lr.append(medias_nli_lr)
+    # Create dataset specific BoW features as new columns
+    all_datasets[name] = np.concatenate( (cv.transform(dataset['left'] + "," + dataset['right']).toarray(), dataset[ALL_FEATURES].values), axis=1)
+    # datasets are saved as numpy arrays from now on
 
-        medias_sts_nb, medias_nli_nb=MachineLearning.DecisionTree(X_train, y_sts, y_nli, col)
-        scores_sts_nb.append(medias_sts_nb)
-        scores_nli_nb.append(medias_nli_nb)
 
-        medias_sts_svm, medias_nli_svm=MachineLearning.SVM(X_train, y_sts, y_nli, col)
-        scores_sts_svm.append(medias_sts_svm)
-        scores_nli_svm.append(medias_nli_svm)
+# Step 4 - create scenarios for crossvalidation
+crossValidation_datasets = list()
 
-#print (scores_sts_svm, scores_nli_svm)
+crossValidation_datasets.append("PhrasIS_train_h_p")
+crossValidation_datasets.append("PhrasIS_train_i_p")
+crossValidation_datasets.append("PhrasIS_train_h_p+PhrasIS_train_h_n")
+crossValidation_datasets.append("PhrasIS_train_i_p+PhrasIS_train_i_n")
+crossValidation_datasets.append("PhrasIS_train_h_p+PhrasIS_train_i_p")
+crossValidation_datasets.append("PhrasIS_train_h_p+PhrasIS_train_i_p+PhrasIS_train_h_n+PhrasIS_train_i_n")
 
-# Step 4 -> evaluate models
-res_sts_acc_de, res_sts_f1_de, res_nli_acc_de, res_nli_f1_de= MachineLearning.calculate(scores_sts_de, scores_nli_de, len(scores))
-print ("\nDecision tree results: ")
-print("STS: %0.3f accuracy" % res_sts_acc_de)
-print("NLI: %0.3f accuracy" % res_nli_acc_de)
-print("STS: %0.3f f1_macro" % res_sts_f1_de)
-print("NLI: %0.3f f1_macro" % res_nli_f1_de)
+all_datasets["PhrasIS_train_h_p+PhrasIS_train_h_n"] = np.concatenate( (all_datasets["PhrasIS_train_h_p"], all_datasets["PhrasIS_train_h_n"]) , axis=0)
+all_datasets["PhrasIS_train_i_p+PhrasIS_train_i_n"] = np.concatenate( (all_datasets["PhrasIS_train_i_p"], all_datasets["PhrasIS_train_i_n"]) , axis=0)
+all_datasets["PhrasIS_train_h_p+PhrasIS_train_i_p"] = np.concatenate( (all_datasets["PhrasIS_train_h_p"], all_datasets["PhrasIS_train_i_p"]) , axis=0)
+all_datasets["PhrasIS_train_h_p+PhrasIS_train_i_p+PhrasIS_train_h_n+PhrasIS_train_i_n"] = np.concatenate( (all_datasets["PhrasIS_train_h_p+PhrasIS_train_h_n"], all_datasets["PhrasIS_train_i_p+PhrasIS_train_i_n"]), axis=0)
 
-res_sts_acc_knn, res_sts_f1_knn, res_nli_acc_knn, res_nli_f1_knn= MachineLearning.calculate(scores_sts_kn, scores_nli_kn, len(scores))
-print ("\nKnn results: ")
-print("STS: %0.3f accuracy" % res_sts_acc_knn)
-print("NLI: %0.3f accuracy" % res_nli_acc_knn)
-print("STS: %0.3f f1_macro" % res_sts_f1_knn)
-print("NLI: %0.3f f1_macro" % res_nli_f1_knn)
+target_sts["PhrasIS_train_h_p+PhrasIS_train_h_n"] = np.concatenate( (target_sts["PhrasIS_train_h_p"], target_sts["PhrasIS_train_h_n"]) , axis=0)
+target_sts["PhrasIS_train_i_p+PhrasIS_train_i_n"] = np.concatenate( (target_sts["PhrasIS_train_i_p"], target_sts["PhrasIS_train_i_n"]) , axis=0)
+target_sts["PhrasIS_train_h_p+PhrasIS_train_i_p"] = np.concatenate( (target_sts["PhrasIS_train_h_p"], target_sts["PhrasIS_train_i_p"]) , axis=0)
+target_sts["PhrasIS_train_h_p+PhrasIS_train_i_p+PhrasIS_train_h_n+PhrasIS_train_i_n"] = np.concatenate( (target_sts["PhrasIS_train_h_p+PhrasIS_train_h_n"], target_sts["PhrasIS_train_i_p+PhrasIS_train_i_n"]), axis=0)
 
-res_sts_acc_lr, res_sts_f1_lr, res_nli_acc_lr, res_nli_f1_lr= MachineLearning.calculate(scores_sts_lr, scores_nli_lr, len(scores))
-print ("\nLinear regresssion results: ")
-print("STS: %0.3f accuracy" % res_sts_acc_lr)
-print("NLI: %0.3f accuracy" % res_nli_acc_lr)
-print("STS: %0.3f f1_macro" % res_sts_f1_lr)
-print("NLI: %0.3f f1_macro" % res_nli_f1_lr)
+target_nli["PhrasIS_train_h_p+PhrasIS_train_h_n"] = np.concatenate( (target_nli["PhrasIS_train_h_p"], target_nli["PhrasIS_train_h_n"]) , axis=0)
+target_nli["PhrasIS_train_i_p+PhrasIS_train_i_n"] = np.concatenate( (target_nli["PhrasIS_train_i_p"], target_nli["PhrasIS_train_i_n"]) , axis=0)
+target_nli["PhrasIS_train_h_p+PhrasIS_train_i_p"] = np.concatenate( (target_nli["PhrasIS_train_h_p"], target_nli["PhrasIS_train_i_p"]) , axis=0)
+target_nli["PhrasIS_train_h_p+PhrasIS_train_i_p+PhrasIS_train_h_n+PhrasIS_train_i_n"] = np.concatenate( (target_nli["PhrasIS_train_h_p+PhrasIS_train_h_n"], target_nli["PhrasIS_train_i_p+PhrasIS_train_i_n"]), axis=0)
 
-res_sts_acc_nb, res_sts_f1_nb, res_nli_acc_nb, res_nli_f1_nb= MachineLearning.calculate(scores_sts_nb, scores_nli_nb, len(scores))
-print ("\nGaussian Naive Bayes results: ")
-print("STS: %0.3f accuracy" % res_sts_acc_nb)
-print("NLI: %0.3f accuracy" % res_nli_acc_nb)
-print("STS: %0.3f f1_macro" % res_sts_f1_nb)
-print("NLI: %0.3f f1_macro" % res_nli_f1_nb)
+crossValidation_models = [
+    tree.DecisionTreeClassifier()
+]
 
-res_sts_acc_svm, res_sts_f1_svm, res_nli_acc_svm, res_nli_f1_svm= MachineLearning.calculate(scores_sts_svm, scores_nli_svm, len(scores))
-print ("\nSupport Vector Machine results: ")
-print("STS: %0.3f accuracy" % res_sts_acc_svm)
-print("NLI: %0.3f accuracy" % res_nli_acc_svm)
-print("STS: %0.3f f1_macro" % res_sts_f1_svm)
-print("NLI: %0.3f f1_macro" % res_nli_f1_svm)
+# todo : fitxategi auxiliarreko sailkatzaileak sartu ere
 
-# Step 5 -> save results
+# Step 5 -> Cross Validate models on NLI
+classification_measures = ['accuracy', 'precision_micro', 'precision_macro','recall_micro','recall_macro','f1_micro','f1_macro']
+result_names = ['test_' + name for name in classification_measures]
+
+
+data = []
+for dataset_name in crossValidation_datasets:
+    for model in crossValidation_models:
+        result = cross_validate(model, all_datasets[dataset_name], target_nli[dataset_name] , cv=5, scoring=classification_measures)
+        results = [result[measure].mean() for measure in result_names]
+        data.append([model, dataset_name] + results)
+
+
+table_results_crossValidation = pd.DataFrame(data, columns = ["Model name", "CV Set"] + result_names)
+print(table_results_crossValidation)
+
+
+# Step 6 -> Cross Validate models on STS
+
+# todo
+# Bileran komentatzeko -> STS regresioa da eta ezin dira NLI (labelak) kasuko ebaluazio metrikak erabili
+# ez dakit scikit-learn en zer dagoen, baino pearson, spearman edo R karratu moduko zerbait behar dugu, regresioa ebaluatzeko metrikak
+
+
+
+# Step 7 -> Final training and evaluation on test set
+
+# todo
+
+# Hemen cross validation en erabili ditugun datu base berberak hartu behar dira, baina train bakarrik egiteko, ebaluatu gabe.
+# ebaluatzeko bere test bersioa sortu beharko dugu
+
