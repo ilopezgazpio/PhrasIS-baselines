6dd75cee7c22e1e0006d113a62f85446c0ce67b9
amaiazarranz <amaia.zarranz@opendeusto.es>
2022-04-01 16:00:06 +0200

    results table

--- 03.launch_embedding_baselines.py
+++ 03.launch_embedding_baselines.py
@@ -6,38 +6,19 @@ import os
 import argparse
 import numpy as np
 import pandas as pd
+import math
 # Evaluate non trainable baselines on a given test set
 # Download fasttext embeddings from https://fasttext.cc/docs/en/crawl-vectors.html
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Evaluate simple STS baselines with word embeddings')
 # parser.add_argument('test', help='test file')
-parser.add_argument('--embeddings', default='src/Embeddings_baselines/fasttext.vec', help='the word embeddings txt file')
+parser.add_argument('--embeddings', choices= ['src/Embeddings_baselines/fasttext.vec', 'src/Embeddings_baselines/fasttext2.vec'], default='src/Embeddings_baselines/fasttext.vec', help='the word embeddings txt file')
 parser.add_argument('--mode', choices=['centroid', 'align'], default='align', help='the scoring model')
 parser.add_argument('--normalize', action='store_true', help='length normalize word embeddings')
 parser.add_argument('--keep_stopwords', action='store_true', help='do not remove stopwords')
 parser.add_argument('--encoding', default='utf-8', help='the character encoding for input (defaults to utf-8)')
 args = parser.parse_args()
-
-# Read embeddings
-f = open(args.embeddings, encoding=args.encoding, errors='surrogateescape')
-words, emb = utils.read_embeddings(f)
-
-# Build word to index map
-word2ind = {word: i for i, word in enumerate(words)}
-
-# Length normalize embeddings
-if args.normalize:
-    emb = utils.length_normalize_embeddings(emb)
-
-# Read data and create partitions
-# src, trg, ref = utils.read_data(open(args.data, encoding=args.encoding, errors='surrogateescape'))
-
-datasetsFolder = "dataset/bin"
-
-if not os.path.exists(datasetsFolder):
-    sys.exit("First preprocess the datasets... Exiting")
-
 
 all_names = [
    'PhrasIS_train_h_n',
@@ -49,6 +30,21 @@ all_names = [
    'PhrasIS_test_i_n',
    'PhrasIS_test_i_p',
 ]
+
+models=[
+   'centroid',
+   'align',
+]
+
+embeddings=[
+   'src/Embeddings_baselines/fasttext.vec',
+   'src/Embeddings_baselines/fasttext2.vec',
+]
+
+datasetsFolder = "dataset/bin"
+
+if not os.path.exists(datasetsFolder):
+    sys.exit("First preprocess the datasets... Exiting")
 
 all_datasets = dict( {name : preprocess_utils.loadDatasetPickle( os.path.join(datasetsFolder, name+".pickle")) for name in all_names })
 
@@ -70,39 +66,57 @@ test_datasets.append("PhrasIS_test_i_p+PhrasIS_test_i_n")
 test_datasets.append("PhrasIS_test_h_p+PhrasIS_test_i_p")
 test_datasets.append("PhrasIS_test_h_p+PhrasIS_test_i_p+PhrasIS_test_h_n+PhrasIS_test_i_n")
 
+results=[]
+for embedding in embeddings:
+    # Read embeddings
+    f = open(embedding, encoding=args.encoding, errors='surrogateescape')
+    words, emb = utils.read_embeddings(f)
 
-for evaluation_dataset in test_datasets:
-    dataset = all_datasets[evaluation_dataset]
+    # Build word to index map
+    word2ind = {word: i for i, word in enumerate(words)}
 
-    src = dataset['left']
-    trg = dataset['right']
-    ref = dataset['STS']
+    # Length normalize embeddings
+    if args.normalize:
+        emb = utils.length_normalize_embeddings(emb)
 
+    for evaluation_dataset in test_datasets:
+        for model in models:
+        
+            dataset = all_datasets[evaluation_dataset]
+    
+            src = dataset['left']
+            trg = dataset['right']
+            ref = dataset['STS']
 
-    # Tokenize
-    src = [utils.tokenize(sent) for sent in src]
-    trg = [utils.tokenize(sent) for sent in trg]
+            # Tokenize
+            src = [utils.tokenize(sent) for sent in src]
+            trg = [utils.tokenize(sent) for sent in trg]
+    
+            # Recase
+            src = [utils.recase(sent, word2ind) for sent in src]
+            trg = [utils.recase(sent, word2ind) for sent in trg]
+    
+            # Strip punctuation
+            src = [utils.strip_punctuation(sent) for sent in src]
+            trg = [utils.strip_punctuation(sent) for sent in trg]
 
-    # Recase
-    src = [utils.recase(sent, word2ind) for sent in src]
-    trg = [utils.recase(sent, word2ind) for sent in trg]
+            # Remove stopwords
+            if not args.keep_stopwords:
+    	        src= [utils.remove_stopwords(sent) for sent in src]
+    	        trg=[utils.remove_stopwords(sent) for sent in trg]
+	
+            # Compute similarities
+            sys = np.zeros(ref.shape)
+            
+            for i in range (ref.shape[0]):
+                if model == 'centroid':
+                    sys[i]=utils.centroid_cosine (src[i], trg[i], emb, word2ind)
+                if model == 'align':
+                    sys [i]= utils.align_score(src[i], trg[i], emb, word2ind)
+                    
+            result=utils.pearson(sys, ref)
+            results.append([embedding, evaluation_dataset,model, result])
 
-    # Strip punctuation
-    src = [utils.strip_punctuation(sent) for sent in src]
-    trg = [utils.strip_punctuation(sent) for sent in trg]
-
-    # Remove stopwords
-    if not args.keep_stopwords:
-        src = [utils.remove_stopwords(sent) for sent in src]
-        trg = [utils.remove_stopwords(sent) for sent in trg]
-
-    # Compute similarities
-    sys = np.zeros(ref.shape)
-    for i in range(ref.shape[0]):
-        if args.mode == 'centroid':
-            sys[i] = utils.centroid_cosine(src[i], trg[i], emb, word2ind)
-        if args.mode == 'align':
-            sys[i] = utils.align_score(src[i], trg[i], emb, word2ind)
-
-    # Compute score
-    print(evaluation_dataset + " : " + str(utils.pearson(sys, ref)))
+table_results=pd.DataFrame (results, columns= ["Embedding", "CV Set", "Model Name", "Result"])
+print ("Table of results")
+print (table_results)
